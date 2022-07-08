65cf97c73b023dcf851149988a824a8f848fbaa0
SantiagoOspinaO <santiago.m200@outlook.es>
2022-07-07 22:25:43 -0500

    Se actualiza el proyecto con algunos requerimientos

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -2,6 +2,9 @@ package calculadora;
 
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
+
+import javax.lang.model.element.Element;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +12,13 @@ public class Calculadora {
 
     private static final Logger logger = Logger.getLogger("Logger");
 
-    List<Integer> numbersA = Arrays.asList(12, 9, 12, 4, 27);
-    List<Integer> numbersB = Arrays.asList(6, 7, 8, 3, 0);
+    List<Integer> numbersA = Arrays.asList(12, 9, 12, 4, 27, 18);
+    List<Integer> numbersB = Arrays.asList(6, 7, 8, 3, 0, 3);
 
     public void sumar() {
         if (numbersA.size() == numbersB.size()) {
             var suma = IntStream.range(0, numbersA.size())
+                    .filter(element -> element > 3)
                     .mapToObj(index -> (numbersA.get(index) + numbersB.get(index)))
                     .toList();
 
@@ -28,6 +32,7 @@ public class Calculadora {
     public void restar() {
         if (numbersA.size() == numbersB.size()) {
             var resta = IntStream.range(0, numbersA.size())
+                    .limit(2)
                     .mapToObj(index -> (numbersA.get(index) - numbersB.get(index)))
                     .toList();
 
@@ -41,6 +46,7 @@ public class Calculadora {
     public void multiplicar() {
         if (numbersA.size() == numbersB.size()) {
             var multiplicacion = IntStream.range(0, numbersA.size())
+                    .distinct()
                     .mapToObj(index -> (numbersA.get(index) * numbersB.get(index)))
                     .toList();
 
@@ -53,14 +59,15 @@ public class Calculadora {
 
     public void dividir() {
         if (numbersA.size() == numbersB.size()) {
-            var divicion = IntStream.range(0, numbersA.size()).mapToObj(index -> {
-
-                if (!numbersB.get(index).equals(0)) {
-                    return (numbersA.get(index) / numbersB.get(index));
-                } else {
-                    return "Indeterminado";
-                }
-            }).toList();
+            var divicion = IntStream.range(0, numbersA.size())
+                .filter(element -> element > 2)
+                .mapToObj(index -> {
+                    if (!numbersB.get(index).equals(0)) {
+                        return (numbersA.get(index) / numbersB.get(index));
+                    } else {
+                        return "Indeterminado";
+                    }
+                }).toList();
 
             var response = "divicion = " + divicion.toString();
             logger.info(response);
@@ -71,13 +78,14 @@ public class Calculadora {
 
     public void modulo() {
         if (numbersA.size() == numbersB.size()) {
-            var modulo = IntStream.range(0, numbersA.size()).mapToObj(index -> {
-
-                if (!numbersB.get(index).equals(0)) {
-                    return (numbersA.get(index) % numbersB.get(index));
-                } else {
-                    return "Indeterminado";
-                }
+            var modulo = IntStream.range(0, numbersA.size())
+                .skip(2)
+                .mapToObj(index -> {
+                    if (!numbersB.get(index).equals(0)) {
+                        return (numbersA.get(index) % numbersB.get(index));
+                    } else {
+                        return "Indeterminado";
+                    }
             }).toList();
 
             var response = "modulo = " + modulo.toString();
