8d7688278a3f992f1c27f4950a66c8d6e109201b
SantiagoOspinaO <santiago.m200@outlook.es>
2022-07-07 20:48:53 -0500

    Se crean las operaciones de la calculadora

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,76 @@
+package calculadora;
+
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculadora {
+
+    private static final Logger logger = Logger.getLogger("Logger");
+
+    List<Integer> numbersA = Arrays.asList(12, 9, 12, 4, 27);
+    List<Integer> numbersB = Arrays.asList(6, 7, 8, 3, 3);
+
+    public void sumar() {
+
+        var suma = IntStream.range(0, numbersA.size())
+                .mapToObj(index -> (numbersA.get(index) + numbersB.get(index)))
+                .toList();
+
+        var response = "suma = " + suma.toString();
+        logger.info(response);
+    }
+
+    public void restar() {
+
+        var resta = IntStream.range(0, numbersA.size())
+                .mapToObj(index -> (numbersA.get(index) - numbersB.get(index)))
+                .toList();
+       
+        var response = "resta = " + resta.toString();
+        logger.info(response);
+    }
+
+    public void multiplicar() {
+
+        var multiplicacion = IntStream.range(0, numbersA.size())
+                .mapToObj(index -> (numbersA.get(index) * numbersB.get(index)))
+                .toList();
+
+        var response = "multiplicacion = " + multiplicacion.toString();
+        logger.info(response);
+    }
+
+    public void dividir() {
+
+        var divicion = IntStream.range(0, numbersA.size()).mapToObj(index -> {
+
+            if (!numbersB.get(index).equals(0)) {
+
+                return (numbersA.get(index) / numbersB.get(index));
+            }
+            return "Indeterminado";
+
+        }).toList();
+
+        var response = "divicion = " + divicion.toString();
+        logger.info(response);
+    }
+
+    public void modulo() {
+    
+        var modulo = IntStream.range(0, numbersA.size()).mapToObj(index -> {
+
+            if (!numbersB.get(index).equals(0)) {
+
+                return (numbersA.get(index) % numbersB.get(index));
+            }
+            return "Indeterminado";
+
+        }).toList();
+
+        var response = "modulo = " + modulo.toString();
+        logger.info(response);
+    }
+}
---
+++ calculadora/Main.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Calculadora calculadora = new Calculadora();
+        
+        calculadora.sumar();
+        calculadora.restar();
+        calculadora.multiplicar();
+        calculadora.dividir();
+        calculadora.modulo();
+    }
+}
